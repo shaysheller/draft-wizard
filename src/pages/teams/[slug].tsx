5eb71d20bdfadb73e7833dc933c4cde5fde1f6b8
Shay Sheller <shaysheller@outlook.com>
2023-08-26 00:09:05 -0700

    added table from headless UI and added slug pages for depth charts

---
+++ src/pages/teams/[slug].tsx
@@ -0,0 +1,39 @@
+import type { GetStaticProps, NextPage } from "next";
+import Head from "next/head";
+import { api } from "~/utils/api";
+import { useRouter } from "next/router";
+
+const DepthChartPage: NextPage<{ name: string }> = ({ name }) => {
+  const router = useRouter();
+  return (
+    <>
+      <Head>
+        <title>{`${name} Depth Chart`}</title>
+      </Head>
+      <div>{router.query.slug}</div>
+    </>
+  );
+};
+
+// export const getStaticProps: GetStaticProps = async (context) => {
+//   const ssg = generateSSGHelper();
+
+//   const id = context.params?.id;
+
+//   if (typeof id !== "string") throw new Error("no id");
+
+//   await ssg.posts.getById.prefetch({ id });
+
+//   return {
+//     props: {
+//       trpcState: ssg.dehydrate(),
+//       id,
+//     },
+//   };
+// };
+
+// export const getStaticPaths = () => {
+//   return { paths: [], fallback: "blocking" };
+// };
+
+export default DepthChartPage;
