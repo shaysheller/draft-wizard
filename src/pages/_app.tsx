33fc7e5c3a8eb3cccd029ee2f2b6101ba2a4cee9
Shay Sheller <shaysheller@outlook.com>
2023-08-26 00:13:15 -0700

    added headless UI table and depth chart slug

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,19 @@
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
+import Head from "next/head";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Draft Wizard</title>
+        <meta name="description" content="All your drafting needs" />
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default api.withTRPC(MyApp);
