2e23c2e73042da591529592454b771970417471a
Shay Sheller <shaysheller@outlook.com>
2023-09-07 15:45:13 -0700

    changed location of toaster to depend on screensize

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,10 +5,45 @@ import Head from "next/head";
 import { draftReducer } from "../reducers";
 import { DraftContext, initialState } from "../context";
 import { Toaster } from "react-hot-toast";
-import { useReducer } from "react";
+import { useReducer, useState, useEffect } from "react";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   const [state, dispatch] = useReducer(draftReducer, initialState);
+
+  const [screenSize, setScreenSize] = useState("");
+
+  useEffect(() => {
+    // Define the Tailwind CSS screen size breakpoints
+    const breakpoints = {
+      sm: 640, // Small screens
+      md: 768, // Medium screens
+      lg: 1024, // Large screens
+      xl: 1280, // Extra-large screens
+    };
+
+    // Function to update the screen size state based on the window's width
+    const updateScreenSize = () => {
+      const width = window.innerWidth;
+      if (width < breakpoints.sm) {
+        setScreenSize("sm");
+      } else if (width < breakpoints.md) {
+        setScreenSize("md");
+      } else if (width < breakpoints.lg) {
+        setScreenSize("lg");
+      } else {
+        setScreenSize("xl");
+      }
+    };
+
+    // Call the update function initially and add a resize event listener
+    updateScreenSize();
+    window.addEventListener("resize", updateScreenSize);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("resize", updateScreenSize);
+    };
+  }, []);
 
   return (
     <>
@@ -18,7 +53,7 @@ const MyApp: AppType = ({ Component, pageProps }) => {
         {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
       <DraftContext.Provider value={{ state, dispatch }}>
-        <Toaster position="bottom-right" />
+        <Toaster position={screenSize === "sm" ? "top-left" : "bottom-right"} />
         <Component {...pageProps} />
       </DraftContext.Provider>
     </>
