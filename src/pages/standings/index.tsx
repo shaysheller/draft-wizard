b1dfb7e75cfe943c43ec38d48892ab0b0dd1adfa
user <user@users-MacBook-Pro.local>
2023-12-29 14:16:03 -0800

    added shadcn table component and configured standings table

--- src/pages/standings/index.tsx
+++ src/pages/standings/index.tsx
@@ -4,6 +4,100 @@ import { useMutation, useQuery } from "@tanstack/react-query";
 import { api } from "~/utils/api";
 import { LoadingSpinner } from "~/components/loading";
 
+import {
+  Table,
+  TableHeader,
+  TableBody,
+  TableFooter,
+  TableRow,
+  TableCell,
+  TableCaption,
+  TableHead,
+} from "~/components/table";
+
+const Standings = () => {
+  const { data, isLoading } = api.team.getAll.useQuery();
+
+  if (isLoading || !data) return <LoadingSpinner />;
+
+  return (
+    <div className="h-full  sm:w-fit md:w-3/4 lg:w-3/4">
+      <Table>
+        <TableCaption>League Standings</TableCaption>
+        <TableHeader className="bg-slate-200">
+          <TableRow className="w-full  ">
+            <TableHead className="w-100px border-r border-solid border-white">
+              &nbsp;
+            </TableHead>
+            <TableHead className="w-100px border-r border-solid border-white">
+              &nbsp;
+            </TableHead>
+            <TableHead className="w-100px border-r border-solid border-white">
+              &nbsp;
+            </TableHead>
+            <TableHead
+              className=" border-b border-r border-solid border-white bg-slate-300 text-center"
+              colSpan={2}
+            >
+              Points
+            </TableHead>
+          </TableRow>
+          <TableRow>
+            <TableHead className="border-r border-solid border-white">
+              Rank
+            </TableHead>
+            <TableHead className="border-r border-solid border-white">
+              Team
+            </TableHead>
+            <TableHead className="border-r border-solid border-white text-right">
+              W-L
+            </TableHead>
+            <TableHead className="border-r border-solid border-white text-center">
+              For
+            </TableHead>
+            <TableHead className="text-center">Against</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {data.map((a, ind) => (
+            <TableRow key={a.id}>
+              <TableCell className="font-medium">{ind + 1}</TableCell>
+              <TableCell>{a.teamName}</TableCell>
+              <TableCell className="w-14">
+                <div className="text-right">{`${a.wins}-${a.losses}`}</div>
+              </TableCell>
+              <TableCell className="text-right">{a.pointsFor}</TableCell>
+              <TableCell className="text-right">{a.pointsAgainst}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
+};
+
+// const Standings = () => {
+//   const { data, isLoading } = api.team.getAll.useQuery();
+//   if (isLoading) return <LoadingSpinner />;
+//   return (
+//     <div className="flex w-full flex-col overflow-y-scroll bg-red-200 text-center">
+//       {data?.map((a) => {
+//         const { teamName, name, wins, losses, pointsFor, pointsAgainst } = a;
+//         return (
+//           <div className="" key={a.id}>
+//             <div>Team Name: {teamName}</div>
+//             <div>Name: {name}</div>
+//             <div>Wins: {wins}</div>
+//             <div>Losses: {losses}</div>
+//             <div>Points For: {pointsFor}</div>
+//             <div>Points Against: {pointsAgainst}</div>
+//           </div>
+//         );
+//       })}
+//     </div>
+//   );
+// };
+
 // const addTeam = (teamName: string, realName: string) => {
 //   api.team.postTeam.mutate()
 // };
@@ -80,28 +174,6 @@ import { LoadingSpinner } from "~/components/loading";
 //     </form>
 //   );
 // };
-
-const Standings = () => {
-  const { data, isLoading } = api.team.getAll.useQuery();
-  if (isLoading) return <LoadingSpinner />;
-  return (
-    <div className="flex w-full flex-col overflow-y-scroll bg-red-200 text-center">
-      {data?.map((a) => {
-        const { teamName, name, wins, losses, pointsFor, pointsAgainst } = a;
-        return (
-          <div className="" key={a.id}>
-            <div>Team Name: {teamName}</div>
-            <div>Name: {name}</div>
-            <div>Wins: {wins}</div>
-            <div>Losses: {losses}</div>
-            <div>Points For: {pointsFor}</div>
-            <div>Points Against: {pointsAgainst}</div>
-          </div>
-        );
-      })}
-    </div>
-  );
-};
 
 export default Standings;
 
